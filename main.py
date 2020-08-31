ac497b4fcdcf15ad551a6e962b1595eca98cf401
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2020-09-01 00:10:41 +0900

    Reform main.py to follow pep8

--- main.py
+++ main.py
@@ -8,21 +8,28 @@ import os
 import base64
 from io import BytesIO
 
+
 def ocr_jpn(filepath):
     filename = os.path.basename(filepath)
     print("読み込み画像: {}".format(filename))
     image = Image.open(filepath)
-    result = pytesseract.image_to_string(image, lang='jpn')  # print ocr text from image
+    # get ocr text from image
+    result = pytesseract.image_to_string(image, lang='jpn')
     result = result.replace(' ', '')
     return image, result
 
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['get'])
 def get():
     name = "Engineers"
-    return render_template('index.html', title='OCR for docs', name=name, flag=False)
+    return render_template(
+        'index.html', title='OCR for docs',
+        name=name, flag=False
+    )
+
 
 @app.route('/', methods=['POST'])
 def post():
@@ -36,19 +43,25 @@ def post():
         image, result = ocr_jpn(filepath)
         os.remove(filepath)
 
+        # Handle image as binary data
         buf = BytesIO()
-        image.save(buf,format="png")
+        image.save(buf, format="png")
         image_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
         image_b64data = "data:image/png;base64,{}".format(image_b64str)
 
     else:
         result == "ファイルが正しく選択されませんでした。"
-    return render_template('index.html', title='flask test', name=name, image_b64data=image_b64data, result=result, flag=True)
+    return render_template(
+        'index.html', title='flask test', name=name,
+        image_b64data=image_b64data, result=result, flag=True
+    )
+
 
 @app.route('/test')
 def good():
     name = "test"
     return name
+
 
 if __name__ == "__main__":
     app.run(debug=True)
