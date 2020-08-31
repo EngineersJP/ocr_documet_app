7b04aa1dacbdfe600ca0bb7b4d5be1214d290979
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2020-08-31 23:30:42 +0900

    Add div showing uploading image

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import pytesseract
 from PIL import Image
 import os
 
+import base64
+from io import BytesIO
+
 def ocr_jpn(filepath):
     filename = os.path.basename(filepath)
     print("読み込み画像: {}".format(filename))
     image = Image.open(filepath)
-    return pytesseract.image_to_string(image, lang='jpn')  # print ocr text from image
+    result = pytesseract.image_to_string(image, lang='jpn')  # print ocr text from image
+    return image, result
 
 
 app = Flask(__name__)
@@ -28,11 +32,17 @@ def post():
         filepath = 'src/img/' + secure_filename(f.filename)
         f.save(filepath)
 
-        result = ocr_jpn(filepath)
+        image, result = ocr_jpn(filepath)
         os.remove(filepath)
+
+        buf = BytesIO()
+        image.save(buf,format="png")
+        image_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
+        image_b64data = "data:image/png;base64,{}".format(image_b64str)
+
     else:
         result == "ファイルが正しく選択されませんでした。"
-    return render_template('index.html', title='flask test', name=name, result=result, flag=True)
+    return render_template('index.html', title='flask test', name=name, image_b64data=image_b64data, result=result, flag=True)
 
 @app.route('/test')
 def good():
